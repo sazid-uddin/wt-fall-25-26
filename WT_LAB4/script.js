6fdc66acaa08c792dd2d58b77c7fecca22beb3fa
Md. Sazid Uddin <sazid.uddin@aiub.edu>
2025-11-12 16:03:06 +0600

    JS Demo

---
+++ WT_LAB4/script.js
@@ -0,0 +1,16 @@
+console.log("test");
+
+// int a = 5; // C++ equivalent
+var a; // declaration
+console.log(a);
+var a = 5; // declaration + assignment
+var a;
+console.log(a);
+a = "five";
+console.log(a);
+var s = "AIUBIAN";
+console.log(s.length);
+// alert("this is alert");
+
+var p1 = document.getElementById("para");
+p1.innerHTML = "Visit this link: <a href='https://google.com'>Google</a>";
